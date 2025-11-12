583fc7d1caef825ea39f79822185738af73d4150
Jinlin Wu <2993376081@qq.com>
2025-11-12 14:01:32 +0800

    添加新的probing配置文件和更新相关脚本

--- scripts_cyberport/srun_probing_cholec.sh
+++ scripts_cyberport/srun_probing_cholec.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=jepa_pretrain         # 作业名
+#SBATCH --job-name=jepa_train          # 作业名
 #SBATCH --output=logs9/%x_%j.out      # 标准输出日志（自动包含作业号）
 #SBATCH --error=logs9/%x_%j.err       # 标准错误日志
 #SBATCH --time=48:00:00               # 最大运行时间
-#SBATCH --partition=AISS2025073101    # 队列（根据集群配置调整）
+#SBATCH --partition=a100               # 队列（根据集群配置调整）
 #SBATCH --nodes=1                     # 节点数量
 #SBATCH --ntasks=1                    # 启动的任务数
-#SBATCH --cpus-per-task=2             # 每个任务的CPU核心数（按需调整）
+#SBATCH --cpus-per-task=8             # 每个任务的CPU核心数（按需调整）
 #SBATCH --gres=gpu:2                  # GPU数量
-#SBATCH --mem=128G                    # 内存大小（按需调整）
+#SBATCH --mem=128G                     # 内存大小（按需调整）
+
 
 # ========================
 # conda 环境准备
 # ========================
 
+# >>> conda initialize >>>
 conda_path="/lustre/projects/med-multi-llm/jinlin_wu/miniconda3"
 
-# >>> conda initialize >>>
 __conda_setup="$('${conda_path}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
@@ -33,29 +34,14 @@ unset __conda_setup
 conda deactivate
 conda activate jepa_torch
 
-
 # ========================
 # 参数解析 & 环境准备
 # ========================
 
 # 传入运行参数
-TASK=multidata_cpt     # 例如：classification 或 segmentation
-# # FNAME=cpt_vitl-256px-64f_lr1e-4_epoch-10.yaml    # 配置文件名，例如 config.yaml
-# # FNAME="cpt_vitl-256px-64f_lr1e-4_epoch-10_neuro.yaml"
-# FNAME="cpt_vitl-256px-64f_lr1e-4_epoch-10_21-dataset_40epoch.yaml"
-# FNAME="cpt_vitl-256px-64f_lr1e-4_epoch-10_21-dataset_80epoch.yaml"
-FNAME="cpt_vith-256px-64f_lr1e-4_epoch-20_21-dataset.yaml"
-# FNAME="cpt_vitg-256px-64f_lr1e-4_epoch-20_21-dataset.yaml"
-
-DEVICES=$(echo $CUDA_VISIBLE_DEVICES | tr ',' ' ')
-echo "DEVICES=${DEVICES}"
-
-
-# TASK="multidata_ntp"
-# FNAME="window_predict_vitl-256px-64f_lr1e-4_epoch-10_21-dataset.yaml"
-# # DEVICES=$CUDA_VISIBLE_DEVICES  # 设备号，例如 0,1
-# DEVICES=$(echo $CUDA_VISIBLE_DEVICES | tr ',' ' ')
-# # echo "DEVICES=${DEVICES}"
+TASK=probing    # 例如：classification 或 segmentation
+FNAME="pitvis_vitl_cpt_attentive_64f.yaml"
+DEVICES=$CUDA_VISIBLE_DEVICES  # 设备号，例如 0,1
 
 # 时间戳
 TIME=$(date +"%Y%m%d_%H%M")
@@ -64,12 +50,13 @@ TIME=$(date +"%Y%m%d_%H%M")
 CFG_NAME=${FNAME%.yaml}
 
 # Slurm 日志路径（独立训练日志）
-LOG_FILE="logs9/${TASK}_${TIME}_${CFG_NAME}.log"
+LOG_FILE="logs/${TASK}_${TIME}_${CFG_NAME}.log"
 
 # 确保日志目录存在
-mkdir -p logs9
+mkdir -p logs10
 
-
+# 设置端口（可根据需要随机分配）
+export MASTER_PORT=${MASTER_PORT:-$((12000 + RANDOM % 20000))}
 
 # ========================
 # 启动训练任务
@@ -82,8 +69,8 @@ echo "DEVICES=${DEVICES}"
 echo "MASTER_PORT=${MASTER_PORT}"
 echo "LOG_FILE=${LOG_FILE}"
 
-
-srun python -m app.main \
+# 启动 Python 程序
+srun python -m evals.main \
   --fname "configs/${TASK}/${FNAME}" \
   --devices ${DEVICES} \
   > "${LOG_FILE}" 2>&1
